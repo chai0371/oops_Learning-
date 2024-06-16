6a94679ed98777bc084e3ab5d7b95adb3947e5d6
Chaitanya Mahmuni <78221851+chai0371@users.noreply.github.com>
2024-06-16 11:40:33 +0530

    Add files via upload

---
+++ practical_1.java
@@ -0,0 +1,8 @@
+package oopsCollage_practicals;
+
+public class practical_1 {
+    public static void main(String[] args) {
+        System.out.println("Name :  Om Balasaheb Kaldate");
+        System.out.println("Hello, World!");
+    }
+}
---
+++ practical_12.java
@@ -0,0 +1,19 @@
+package oopsCollage_practicals;
+
+public class practical_12 {
+    public static void main(String[] args) {
+//        System.out.println("Name :  Chaitanya Mahamuni");
+//        System.out.println();
+//
+//        try {int[] numbers = {1, 2, 3};
+//            System.out.println("Accessing element at index 3: " + numbers[5]);
+//        } catch(ArrayIndexOutOfBoundsException e){
+//            System.out.println("An ArrayIndexOutOfBoundsException occurred: " + e.getMessage());
+//        }
+//        finally {
+//            System.out.println("This block is always executed.");
+//        }
+
+        System.out.println(-1%3);
+    }
+}
